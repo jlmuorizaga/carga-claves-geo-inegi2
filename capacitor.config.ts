4ece7bc362a16173ba56025e1d59d6a34dfbe537
JOSE LUIS MU ORIZAGA <jlmuo@yahoo.com>
2024-09-09 13:21:23 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'carga-claves-geo-inegi2',
+  webDir: 'www'
+};
+
+export default config;
